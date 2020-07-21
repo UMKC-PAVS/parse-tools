97278dde71ba6f8a202ea481e1ba53a50e0c54a1
HarrTerr <62953870+HarrTerr@users.noreply.github.com>
2020-07-21 15:17:37 -0500

    Simplification of previous code

--- px4/Analysis Combiners.py
+++ px4/Analysis Combiners.py
@@ -13,31 +13,23 @@ Not ready for running
 
 import os
 import shutil
-import glob
 import pandas as pd
 
 #Overarching folder that holds subfolders with combined files
 src_dir = r'C:\Users\het9t\OneDrive\Documents\PX4 Summary Statistics Sheets\075'
 #Directory for degradation files
 dst_dir= r'C:\Users\het9t\OneDrive\Documents\PX4 Summary Statistics Sheets\complete'
+
+files_list = []
 for root, dirs, files in os.walk(src_dir):
     for f in files:
         if f.endswith('Data.csv'):
-            shutil.copy(os.path.join(root,f), dst_dir)
+            df3 = pd.read_csv(files,index_col=None, header=0)
+            files_list.append(df3)            
         #if f.endswith('Statistics.csv'):
          #   shutil.copy(os.path.join(root,f), dst_dir)
-            
-os.chdir(dst_dir)
-
-all_files = glob.glob(dst_dir + "/*.csv")
-
-lis = []
-
-for filename in all_files:
-    df = pd.read_csv(filename, index_col=None, header=0)
-    lis.append(df)
-    
-frame = pd.concat(lis)
+         
+frame = pd.concat(files_list)
 
 frame = frame.drop(['Unnamed: 0'],axis=1)
 
