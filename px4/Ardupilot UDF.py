a6e277cdb776d2f9777ee55f5240c6c3c53ef23b
HarrTerr <62953870+HarrTerr@users.noreply.github.com>
2020-07-31 21:03:45 -0500

    Full list

--- px4/Ardupilot UDF.py
+++ px4/Ardupilot UDF.py
@@ -6,20 +6,20 @@ import numpy as np
 
 
 
-list_of_filenames = [f for f in os.listdir('C:\\Users\\het9t\\anaconda3\\testFiles\\') if f.endswith('.csv')]
+list_of_filenames = [f for f in os.listdir(r'C:\Users\het9t\OneDrive\Documents\PX4 Summary Statistics Sheets\Ardupilot') if f.endswith('.csv')]
 list_of_df = []
 
-dont_reject_list = ['BARO','BAT','CTUN','GPS','IOMC','IMU','MAG','NTUN','POWR','RCIN','STAT','RSSI']
+dont_reject_list = ['AETR','AHR2','BARO','MODE','BAT','CTUN','GPA','GPS','RAD','IOMC','IMU','MAG','NTUN','POWR','RCIN','STAT','RSSI']
 
 list_of_filenames = [item for item in list_of_filenames if any(word in item for word in dont_reject_list)]
 
+os.chdir(r'C:\Users\het9t\OneDrive\Documents\PX4 Summary Statistics Sheets\Ardupilot')
 
 for current_filename in list_of_filenames:
     
     print('current_filename: '+current_filename)
     
     df = pd.read_csv(current_filename, index_col=1, header=0)
-            
             # create list of df columns
     list_of_column_names = list(df.columns)
     list_of_new_column_names = []
@@ -27,6 +27,13 @@ for current_filename in list_of_filenames:
 
             # store the current df (from a single csv) into the big list of dfs
     list_of_df.append(df)
+
+
+rcou = [f for f in os.listdir(r'C:\Users\het9t\OneDrive\Documents\PX4 Summary Statistics Sheets\Ardupilot') if f.endswith('RCOU.csv')]
+df = pd.read_csv(rcou[0],index_col=1,header=0)
+df = df.rename(columns={'C1':'RCOUTC1','C2':'RCOUTC2','C3':'RCOUTC3','C4':'RCOUTC4','C5':'RCOUTC5','C6':'RCOUTC6','C7':'RCOUTC7','C8':'RCOUTC8','C9':'RCOUTC9','C10':'RCOUTC10','C11':'RCOUTC11','C12':'RCOUTC12','C13':'RCOUTC13','C14':'RCOUTC14',})
+list_of_df.append(df)    
+
 
         # create the big df by using the concat method called on a list of small df
 big_df = pd.concat(list_of_df, axis=0, ignore_index=False, sort=False)
@@ -79,23 +86,23 @@ resampled_df = big_df.asfreq(str(freq_arg)+freq_type,method='ffill')
 resampled_df.index = resampled_df.index.values.astype(np.uint64)/1000
 
         # get rid of the unused column before we send it to csv
-resampled_df = resampled_df.drop(columns=['time_properformat','VoltR','Curr','CurrTot','EnrgTot','Res','NavRoll','NavPitch','ThrOut','RdrOut','Aspd','OfsX','OfsY','OfsZ','MOfsX','MOfsY','MOfsZ','S','WpDist','TBrg','NavBrg','AltErr','XT','XTi','ThrDem','Press','CRt','GndTemp', 'Temp' ,'Status' , 'GMS','GWk','NSats', 'U', 'Spd','VZ','EG','EA','T','GH','AH','GHz','AHz','GCrs','ArspdErr','TLat','TLng','TAlt','Vcc','VServo','Flags','Safety','C5','C6','C7','C8','C9','C10','C11','C12','C13','C14','Hit','Stage','Still','Armed','Crash','isFlyProb','isFlying','SMS','Offset','Roll','Pitch'])
+resampled_df = resampled_df.drop(columns=['SAcc','Rsn','ModeNum','Q1','Q2','Q3','Q4','RemRSSI','TxBuf','VV','Delta','RemNoise','Noise','RxErrors','Fixed','VAcc','HAcc','time_properformat','VoltR','Curr','CurrTot','EnrgTot','Res','NavRoll','NavPitch','ThrOut','RdrOut','Aspd','OfsX','OfsY','OfsZ','MOfsX','MOfsY','MOfsZ','S','WpDist','TBrg','NavBrg','AltErr','XT','XTi','ThrDem','Press','CRt','GndTemp', 'Temp' ,'Status' , 'GMS','GWk','NSats', 'U', 'Spd','VZ','EG','EA','T','GH','AH','GHz','AHz','GCrs','ArspdErr','TLat','TLng','TAlt','Vcc','VServo','Flags','Safety','Hit','Stage','Still','Armed','Crash','isFlyProb','isFlying','SMS','Offset'])
 
-resampled_df = resampled_df.rename(columns={"Health":"RCfailsafe","C1":"Roll","C2":"Pitch","C3":"Throttle","C4":"Yaw","Lat":"GPS.Lat","Lng":"GPS.Lon","Alt":"GPS.alt","GyrX" : "GyroX", "GyrY" : "GyroY", "GyrZ" : "GyroZ","AccZ": "AccelZ", "AccX":"AccelX","AccY":"AccelY"})
+resampled_df = resampled_df.rename(columns={"Health":"RCfailsafe",'C3':'C3:Throttle',"Lat":"GPS.Lat","Lng":"GPS.Lon","Alt":"GPS.Alt","GyrX" : "GyroX", "GyrY" : "GyroY", "GyrZ" : "GyroZ","AccZ": "AccelZ", "AccX":"AccelX","AccY":"AccelY"})
 
 
 #Normalize GPS signals 
 xx = resampled_df['GPS.Lat'].iloc[0]
 yy = resampled_df['GPS.Lon'].iloc[0]
-zz = resampled_df['GPS.alt'].iloc[0]
+zz = resampled_df['GPS.Alt'].iloc[0]
 
 resampled_df['GPS.Lat'] = resampled_df["GPS.Lat"] - xx
 
 resampled_df['GPS.Lon'] = resampled_df["GPS.Lon"] - yy
 
-resampled_df['GPS.alt'] = resampled_df["GPS.alt"] - zz
+#resampled_df['GPS.alt'] = resampled_df["GPS.alt"] - zz
 
 #resampled_df = resampled_df.sub([1], resampled_df[1,0])
 
 #send it to CSV
-resampled_df.to_csv('UDF.csv')
+#resampled_df.to_csv('UDF.csv')
