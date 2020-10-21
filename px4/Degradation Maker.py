37bfa2f93a10ffaf1b1258cd169703372652c100
HarrTerr <62953870+HarrTerr@users.noreply.github.com>
2020-10-20 22:29:52 -0500

    Update Degradation Maker.py

--- px4/Degradation Maker.py
+++ px4/Degradation Maker.py
@@ -27,31 +27,38 @@ Removes csv writing in favor of xlsx writing
 Removes lots of no longer used code and general tidiness improvements WIP
 Leaves in call function for ulog2csv, may remove in future updates
 
+10/16/20 Complete Rewrite so it can be used easier by other people
+
+10/20/20 Typos
 
 """
+
+'''
+README: This is written for the PX4 platform, which has different timing for magnetometer versus the accelerometer and gyroscope. This would be easier in other platforms such as DJI which has a unified timing structure.
+
+The mean and stdev have two separate arrays which get combined due to this different timing. It probably could be done together but would need yet another rewrite which isn't likely to be necessary at this time'
+'''
+
+
+
 
 
 import os
 import pandas as pd
 
-
-#NOTE: If you do not use forward slashes instead of backslashes you must put it in the format of r'path' with no backslashes at the end of the path. Python will give you an error otherwise due to unicode
-#directorypath = r'C:\Users\cuav\Documents\Python Scripts\071'
-directorypath = r'C:\Users\het9t\OneDrive\Documents\PX4 Summary Statistics Sheets\complete'
+directorypath = input("Please input directory of files needed. If files are named incorrectly then please use renaming file: ")
 os.chdir(directorypath)
 
-#Set to 0 if group, sequential and rep need to be added
+platform = input("Please input the vehicle number and platform the files start with (example PX4-075-) ")
 
 file_list = []
 file_list2 = []
 averages = []
 deviations = []
 
-qq = 0
-if qq == 0:
-    for file in os.listdir(directorypath):
-        if file.startswith('PX4-075'):
-            if file.endswith('.csv'):
+for file in os.listdir(directorypath):
+    if file.startswith(platform):
+        if file.endswith('.csv'):
                 df3 = pd.read_csv(file)
             
                 df = df3[['gyro_rad[0]','gyro_rad[1]','gyro_rad[2]','accelerometer_m_s2[0]','accelerometer_m_s2[1]','accelerometer_m_s2[2]']]
@@ -60,11 +67,11 @@ if qq == 0:
                 df2 = df3[['magnetometer_ga[0]','magnetometer_ga[1]','magnetometer_ga[2]']]
                 df2 = df2.dropna()
 
-            
-            
+#Finds the length of the array for sequential purposes
                 L = len(df.index)
                 LL = len(df2.index)
             
+#Current naming structure has the group on the 8th index of the string and the repetition number on the 11th index. This could be changed in the future, and may need to be adjusted for that change
                 Group2 = [float(file[8])] * L
                 Group3= [float(file[8])] * LL
                 names = float(file[11])
@@ -98,31 +105,20 @@ if qq == 0:
                 mean2.columns = ['Mean']
                 stdev2.columns = ['Stdev']
                 
-                
-                
                 mean = mean.transpose()
                 mean2 = mean2.transpose()
-                #mean = mean[['Group','Seq','Rep','sensor_combined_0_gyro_rad[0]','sensor_combined_0_gyro_rad[1]','sensor_combined_0_gyro_rad[2]','sensor_combined_0_accelerometer_m_s2[0]','sensor_combined_0_accelerometer_m_s2[1]','sensor_combined_0_accelerometer_m_s2[2]','vehicle_magnetometer_0_magnetometer_ga[0]','vehicle_magnetometer_0_magnetometer_ga[1]','vehicle_magnetometer_0_magnetometer_ga[2]']]
-                #mean = mean.rename(columns= {'sensor_combined_0_gyro_rad[0]':'GyroX','sensor_combined_0_gyro_rad[1]':'GyroY','sensor_combined_0_gyro_rad[2]':'GyroZ','sensor_combined_0_accelerometer_m_s2[0]':'AccelX','sensor_combined_0_accelerometer_m_s2[1]':'AccelY','sensor_combined_0_accelerometer_m_s2[2]':'AccelZ','vehicle_magnetometer_0_magnetometer_ga[0]':'MagX','vehicle_magnetometer_0_magnetometer_ga[1]':'MagY','vehicle_magnetometer_0_magnetometer_ga[2]':'MagZ'})
-                mean = mean[['Group','Seq','Rep','gyro_rad[0]','gyro_rad[1]','gyro_rad[2]','accelerometer_m_s2[0]','accelerometer_m_s2[1]','accelerometer_m_s2[2]']]
-                mean2 = mean2[['Group','Seq','Rep','magnetometer_ga[0]','magnetometer_ga[1]','magnetometer_ga[2]']]
+                mean = mean[['Group','Rep','gyro_rad[0]','gyro_rad[1]','gyro_rad[2]','accelerometer_m_s2[0]','accelerometer_m_s2[1]','accelerometer_m_s2[2]']]
+                mean2 = mean2[['Group','Rep','magnetometer_ga[0]','magnetometer_ga[1]','magnetometer_ga[2]']]
                 mean = mean.rename(columns= {'gyro_rad[0]':'GyroX','gyro_rad[1]':'GyroY','gyro_rad[2]':'GyroZ','accelerometer_m_s2[0]':'AccelX','accelerometer_m_s2[1]':'AccelY','accelerometer_m_s2[2]':'AccelZ'})
                 mean2 = mean2.rename(columns= {'magnetometer_ga[0]':'MagX','magnetometer_ga[1]':'MagY','magnetometer_ga[2]':'MagZ'})
-                mean = mean.drop(columns=['Seq'])
-                mean2 = mean2.drop(columns=['Seq'])
-                
-                
+            
                 stdev = stdev.transpose()
                 stdev2 = stdev2.transpose()
-                #stdev = stdev[['Group','Seq','Rep','sensor_combined_0_gyro_rad[0]','sensor_combined_0_gyro_rad[1]','sensor_combined_0_gyro_rad[2]','sensor_combined_0_accelerometer_m_s2[0]','sensor_combined_0_accelerometer_m_s2[1]','sensor_combined_0_accelerometer_m_s2[2]','vehicle_magnetometer_0_magnetometer_ga[0]','vehicle_magnetometer_0_magnetometer_ga[1]','vehicle_magnetometer_0_magnetometer_ga[2]']]
-                #stdev = stdev.rename(columns= {'sensor_combined_0_gyro_rad[0]':'GyroX','sensor_combined_0_gyro_rad[1]':'GyroY','sensor_combined_0_gyro_rad[2]':'GyroZ','sensor_combined_0_accelerometer_m_s2[0]':'AccelX','sensor_combined_0_accelerometer_m_s2[1]':'AccelY','sensor_combined_0_accelerometer_m_s2[2]':'AccelZ','vehicle_magnetometer_0_magnetometer_ga[0]':'MagX','vehicle_magnetometer_0_magnetometer_ga[1]':'MagY','vehicle_magnetometer_0_magnetometer_ga[2]':'MagZ'})
-                stdev = stdev[['Group','Seq','Rep','gyro_rad[0]','gyro_rad[1]','gyro_rad[2]','accelerometer_m_s2[0]','accelerometer_m_s2[1]','accelerometer_m_s2[2]']]
-                stdev2 = stdev2[['Group','Seq','Rep','magnetometer_ga[0]','magnetometer_ga[1]','magnetometer_ga[2]']]
+                stdev = stdev[['Group','Rep','gyro_rad[0]','gyro_rad[1]','gyro_rad[2]','accelerometer_m_s2[0]','accelerometer_m_s2[1]','accelerometer_m_s2[2]']]
+                stdev2 = stdev2[['Group','Rep','magnetometer_ga[0]','magnetometer_ga[1]','magnetometer_ga[2]']]
                 stdev = stdev.rename(columns= {'gyro_rad[0]':'GyroX','gyro_rad[1]':'GyroY','gyro_rad[2]':'GyroZ','accelerometer_m_s2[0]':'AccelX','accelerometer_m_s2[1]':'AccelY','accelerometer_m_s2[2]':'AccelZ'})
                 stdev2 = stdev2.rename(columns= {'magnetometer_ga[0]':'MagX','magnetometer_ga[1]':'MagY','magnetometer_ga[2]':'MagZ'})
                 
-                stdev = stdev.drop(columns=['Seq'])
-                stdev2 = stdev2.drop(columns=['Seq'])
                 
                 stdev['Group'] = mean['Group'].values
                 stdev['Rep'] = mean['Rep'].values
@@ -157,29 +153,16 @@ data2 = pd.concat(file_list2,ignore_index=True)
 Aves = pd.concat(averages,ignore_index=True)
 Devs = pd.concat(deviations,ignore_index=True)
 
-#For Gyro and Accelerometer Data
-#df = data[['Group','Seq','Rep','sensor_combined_0_gyro_rad[0]','sensor_combined_0_gyro_rad[1]','sensor_combined_0_gyro_rad[2]','sensor_combined_0_accelerometer_m_s2[0]','sensor_combined_0_accelerometer_m_s2[1]','sensor_combined_0_accelerometer_m_s2[2]']]
-#df = data[['Group','Seq','Rep','gyro_rad[0]','gyro_rad[1]','gyro_rad[2]','accelerometer_m_s2[0]','accelerometer_m_s2[1]','accelerometer_m_s2[2]']]
-#df = df.rename(columns= {'sensor_combined_0_gyro_rad[0]':'GyroX','sensor_combined_0_gyro_rad[1]':'GyroY','sensor_combined_0_gyro_rad[2]':'GyroZ','sensor_combined_0_accelerometer_m_s2[0]':'AccelX','sensor_combined_0_accelerometer_m_s2[1]':'AccelY','sensor_combined_0_accelerometer_m_s2[2]':'AccelZ'})
-
-
-#For Magnetometer Data
-#df2 = data[['Group','Seq','Rep','vehicle_magnetometer_0_magnetometer_ga[0]','vehicle_magnetometer_0_magnetometer_ga[1]','vehicle_magnetometer_0_magnetometer_ga[2]']]
-#df2 = data[['Group','Seq','Rep','magnetometer_ga[0]','magnetometer_ga[1]','magnetometer_ga[2]']]
-#df2 = df2.rename(columns= {'vehicle_magnetometer_0_magnetometer_ga[0]':'MagX','vehicle_magnetometer_0_magnetometer_ga[1]':'MagY','vehicle_magnetometer_0_magnetometer_ga[2]':'MagZ'})
-
-
 
 path = os.getcwd()
 directory = os.path.join(path,r'Degradation Data')
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-
 #Changes directory to put it into the new folder
 os.chdir(directory)
 
-Comb_Data = pd.ExcelWriter('PX4-075-Data.xlsx', engine='xlsxwriter')
+Comb_Data = pd.ExcelWriter(platform + "Data.csv", engine='xlsxwriter')
 data.to_excel(Comb_Data,sheet_name='Accel&Gyro',index=False)
 data2.to_excel(Comb_Data,sheet_name='Magnetometer',index=False)
 Aves.to_excel(Comb_Data,sheet_name='Mean',index=False)
